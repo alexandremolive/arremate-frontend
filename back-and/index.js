d957edc7d99b50f57fa92d3721007261675813eb
rogerio-lambert <rogerio.cl@gmail.com>
2021-10-25 18:20:41 -0300

    criação de socket dentro do index

--- back-and/index.js
+++ back-and/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http').createServer(app);
+let products = require('./products.js');
 
 const io = require('socket.io')(http, {
   cors: {
@@ -9,14 +10,27 @@ const io = require('socket.io')(http, {
     methods: ['GET', 'POST'], // Métodos aceitos pela url
   }});
 
-  let corsOptions = {
-    origin: 'http://localhost:3000',
-  };
+let corsOptions = {
+  origin: 'http://localhost:3000',
+};
 
-  const getProductsSocket = require('./sockets/buyers');
-  getProductsSocket(io);
-  
-  app.use(cors(corsOptions));
+
+io.on('connection', (socket) => {
+  // console.log(`Cliente ${socket.id} acabou de entrar`);
+  socket.on('updateValue',  (id, lance) => {
+    let productToUpdate = products.find((product) => product.id === id);
+    products = products.find((product) => product.id !== id);
+    productToUpdate.value = productToUpdate.value + lance;
+    productToUpdate.push(productToUpdate)
+    io.emit('sendProducts', products);
+  })
+});
+
+
+const getProductsSocket = require('./sockets/buyers');
+getProductsSocket(io);
+
+app.use(cors(corsOptions));
 
 
 http.listen(3000, () => {
